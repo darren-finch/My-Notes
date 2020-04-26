2a151564fcd9188987a670cbd98bcf0a88c85d50
Darren <finchdarren25@gmail.com>
2020-04-25 20:06:13 -0500

    Starting point v3.

--- app/src/main/java/com/all/mynotes/fragments/AddEditNoteFragment.kt
+++ app/src/main/java/com/all/mynotes/fragments/AddEditNoteFragment.kt
@@ -1,30 +1,21 @@
 package com.all.mynotes.fragments
 
 import android.os.Bundle
-import android.text.Editable
 import android.view.*
-import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.all.mynotes.MainActivity
-import com.all.mynotes.NotesViewModel
 import com.all.mynotes.R
-import com.all.mynotes.data.NoteModel
-import com.all.mynotes.helpers.DependencyInjector
 import com.google.android.material.textfield.TextInputEditText
 
 class AddEditNoteFragment : Fragment()
 {
-    private lateinit var notesViewModel: NotesViewModel
-
     private lateinit var noteTitleEditText: TextInputEditText
     private lateinit var noteDescriptionEditText: TextInputEditText
     private lateinit var notePriorityEditText: TextInputEditText
 
     private var noteKey = "" //This shouldn't change even though its a var
-    private lateinit var noteData: NoteModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
     {
@@ -39,21 +30,6 @@ class AddEditNoteFragment : Fragment()
     override fun onActivityCreated(savedInstanceState: Bundle?)
     {
         super.onActivityCreated(savedInstanceState)
-        notesViewModel = ViewModelProvider(requireActivity(), DependencyInjector.provideNoteViewModelFactory()).get(NotesViewModel::class.java)
-
-        if(noteKey.isNotEmpty())
-        {
-            (requireActivity() as AppCompatActivity).supportActionBar?.title = "Edit Note"
-            notesViewModel.getNote(noteKey).observe(viewLifecycleOwner, Observer { data ->
-                noteData = data
-                initUIData()
-            })
-        }
-        else
-        {
-            (requireActivity() as AppCompatActivity).supportActionBar?.title = "Add Note"
-            noteData = NoteModel()
-        }
 
         initUI()
     }
@@ -64,12 +40,6 @@ class AddEditNoteFragment : Fragment()
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean
     {
-        when (item.itemId)
-        {
-            R.id.saveButton -> if (noteKey.isEmpty()) insertNote() else updateNote()
-            R.id.deleteButton -> deleteNote()
-        }
-        navigateToAllNotesFragment()
         return false
     }
     private fun initUI()
@@ -78,35 +48,9 @@ class AddEditNoteFragment : Fragment()
         noteDescriptionEditText = requireActivity().findViewById(R.id.noteDescriptionEditText)
         notePriorityEditText = requireActivity().findViewById(R.id.notePriorityEditText)
     }
-    private fun initUIData()
-    {
-        noteTitleEditText.setText(noteData.title)
-        noteDescriptionEditText.setText(noteData.description)
-        notePriorityEditText.setText(noteData.priority.toString())
-    }
     private fun setNoteKey(noteKey: String)
     {
         this.noteKey = noteKey
-    }
-    private fun saveNoteData()
-    {
-        noteData.title = noteTitleEditText.text.toString()
-        noteData.description = noteDescriptionEditText.text.toString()
-        noteData.priority = notePriorityEditText.text.toString().toInt()
-    }
-    private fun insertNote()
-    {
-        saveNoteData()
-        notesViewModel.insertNote(noteData)
-    }
-    private fun updateNote()
-    {
-        saveNoteData()
-        notesViewModel.updateNote(noteData)
-    }
-    private fun deleteNote()
-    {
-        notesViewModel.deleteNote(noteData.key)
     }
     private fun navigateToAllNotesFragment()
     {
@@ -115,7 +59,6 @@ class AddEditNoteFragment : Fragment()
             .replace(R.id.container, AllNotesFragment())
             .commitNow()
     }
-
     companion object
     {
         fun newInstance(noteKey: String) : AddEditNoteFragment
--- app/src/main/java/com/all/mynotes/fragments/AllNotesFragment.kt
+++ app/src/main/java/com/all/mynotes/fragments/AllNotesFragment.kt
@@ -9,32 +9,13 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.all.mynotes.MainActivity
-import com.all.mynotes.NotesViewModel
 
 import com.all.mynotes.R
-import com.all.mynotes.data.NoteModel
-import com.all.mynotes.helpers.DependencyInjector
 import com.all.mynotes.recyclerviews.MarginItemDecoration
-import com.all.mynotes.recyclerviews.NotesRecyclerViewAdapter
-import com.all.mynotes.recyclerviews.ViewHolderEventListener
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class AllNotesFragment : Fragment()
 {
-    private lateinit var notesViewModel: NotesViewModel
-
-    private lateinit var notesRecyclerView: RecyclerView
-    private lateinit var notesRecyclerViewAdapter: NotesRecyclerViewAdapter
-
-    private val onNoteViewHolderEventListener = object : ViewHolderEventListener
-    {
-        override fun onClicked(noteKey: String)
-        {
-            navigateToAddEditNoteFragment(noteKey)
-        }
-    }
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
     {
         // Inflate the layout for this fragment
@@ -45,25 +26,13 @@ class AllNotesFragment : Fragment()
     {
         super.onActivityCreated(savedInstanceState)
 
-        notesViewModel = ViewModelProvider(requireActivity(), DependencyInjector.provideNoteViewModelFactory()).get(NotesViewModel::class.java)
-        notesViewModel.getNotes().observe(viewLifecycleOwner, Observer { notes ->
-            notesRecyclerViewAdapter.updateNotes(notes)
-        })
-
         val addNewNoteFab: FloatingActionButton = requireView().findViewById(R.id.addNewNote)
         addNewNoteFab.setOnClickListener{
             navigateToAddEditNoteFragment("")
         }
-
-        notesRecyclerViewAdapter = NotesRecyclerViewAdapter(mutableListOf(), onNoteViewHolderEventListener)
-
-        notesRecyclerView = view!!.findViewById(R.id.notesRecyclerView)
-        notesRecyclerView.layoutManager = LinearLayoutManager(context)
-        notesRecyclerView.addItemDecoration(MarginItemDecoration(16))
-        notesRecyclerView.adapter = notesRecyclerViewAdapter
     }
 
-    fun navigateToAddEditNoteFragment(noteKey: String)
+    private fun navigateToAddEditNoteFragment(noteKey: String)
     {
         requireActivity().supportFragmentManager
             .beginTransaction()
